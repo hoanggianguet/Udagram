7fbf56c37afdc8e899c42417fdcd780d51401d5a
hoanggianguet <gianghoang.uet.vnu@gmail.com>
2023-06-02 18:34:26 +0700

    Update Typescipt check for Request and Respose

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
@@ -28,7 +28,7 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-  app.get("/filteredimage", async (req, res) => {
+  app.get("/filteredimage", async (req: Request, res: Response) => {
     const imageUrl: string = req.query.image_url.toString();
     if (!imageUrl) {
       return res.status(400).send({
